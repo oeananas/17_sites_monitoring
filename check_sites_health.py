117ff6c6873c7a75bf197f2776dcfc9e6f0f625e
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-27 13:43:47 +0300

    Update check_sites_health.py
    simplified operation of functions, fixed error handling

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,8 +1,7 @@
-import sys
-import os
 import datetime
 import requests
 import whois
+import argparse
 from urllib.parse import urlparse
 
 
@@ -41,6 +40,14 @@ def is_expiration_date_ok(days_before_expiration, days_limit=30):
     return days_before_expiration > days_limit
 
 
+def get_parsed_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath',
+                        type=str,
+                        help='path to text-file with urls')
+    return parser.parse_args()
+
+
 def print_check_message(condition_stat, condition_exp, domain, days_before_exp):
     if condition_stat and condition_exp:
         check_value = 'SUCCESS!'
@@ -55,25 +62,25 @@ def print_check_message(condition_stat, condition_exp, domain, days_before_exp):
 
 if __name__ == '__main__':
     separator = '*' * 30
-    if len(sys.argv) == 1:
-        exit('need path to file as parameter')
-    filepath = sys.argv[1]
-    if not os.path.isfile(filepath):
-        exit('incorrect path to file')
-    urls_for_check_list = load_urls4check(filepath)
+    arg = get_parsed_argument()
+    filepath = arg.filepath
+    try:
+        urls_for_check_list = load_urls4check(filepath)
+    except(FileNotFoundError, UnicodeDecodeError):
+        exit('incorrect path to file / not a text file')
     for url in urls_for_check_list:
         try:
             condition_status = is_server_respond_ok(url)
+            domain = get_domain_name(url)
+            exp_date = get_domain_expiration_date(domain)
+            days_before_exp = get_days_before_expiration(exp_date)
+            condition_expiration = is_expiration_date_ok(days_before_exp)
+            print_check_message(
+                condition_status,
+                condition_expiration,
+                domain,
+                days_before_exp
+            )
         except requests.exceptions.RequestException:
             print('error for get a response from server')
-        domain = get_domain_name(url)
-        exp_date = get_domain_expiration_date(domain)
-        days_before_exp = get_days_before_expiration(exp_date)
-        condition_expiration = is_expiration_date_ok(days_before_exp)
-        print_check_message(
-            condition_status,
-            condition_expiration,
-            domain,
-            days_before_exp
-        )
         print(separator)
