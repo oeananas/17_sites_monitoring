175171feee7301901adffe78ea10496418e7029f
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-27 20:40:17 +0300

    Update check_sites_health.py
    changed the names of variables, the verification condition is corrected

--- check_sites_health.py
+++ check_sites_health.py
@@ -45,21 +45,29 @@ def is_expiration_date_ok(days_before_expiration, days_limit=30):
 
 def get_parsed_argument():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filepath',
-                        type=str,
-                        help='path to text-file with urls')
+    parser.add_argument('filepath', type=str, help='path to text-file')
     return parser.parse_args()
 
 
-def print_check_message(condition_stat, condition_exp, domain, days_before_exp):
-    if condition_stat and condition_exp:
-        check_val = 'SUCCESS!'
-        response = 'OK'
+def print_check_message(
+        is_server_respond,
+        is_exp_date_correct,
+        domain, days_before_exp
+):
+    if is_server_respond and is_exp_date_correct:
+        general_check_status = 'SUCCESS!'
+        server_response_status = 'OK'
+    elif is_server_respond and not is_exp_date_correct:
+        general_check_status = 'FAILED!'
+        server_response_status = 'OK'
     else:
-        check_val = 'FAILED!'
-        response = 'NO RESPONSE'
-    print('for domain "{}"  :  check status: {}'.format(domain, check_val))
-    print('server response : {}'.format(response))
+        general_check_status = 'FAILED!'
+        server_response_status = 'NO RESPONSE'
+    print('for domain "{}"  :  check status: {}'.format(
+        domain,
+        general_check_status
+    ))
+    print('server response : {}'.format(server_response_status))
     print('days before expiration : {}'.format(days_before_exp))
 
 
@@ -72,14 +80,14 @@ if __name__ == '__main__':
     except(FileNotFoundError, UnicodeDecodeError):
         exit('incorrect path to file / not a text file')
     for url in urls_for_check_list:
-        condition_status = is_server_respond_ok(url)
+        is_server_respond = is_server_respond_ok(url)
         domain = get_domain_name(url)
         exp_date = get_domain_expiration_date(domain)
         days_before_exp = get_days_before_expiration(exp_date)
-        condition_expiration = is_expiration_date_ok(days_before_exp)
+        is_exp_date_correct = is_expiration_date_ok(days_before_exp)
         print_check_message(
-            condition_status,
-            condition_expiration,
+            is_server_respond,
+            is_exp_date_correct,
             domain,
             days_before_exp
         )
