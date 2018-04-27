567d56e00ad9ff13c726f52ed07b8b38888cd388
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-27 13:09:28 +0300

    Update check_sites_health.py
    functions work corrected

--- check_sites_health.py
+++ check_sites_health.py
@@ -13,10 +13,7 @@ def load_urls4check(filepath):
 
 
 def is_server_respond_ok(url):
-    try:
-        return requests.get(url).ok
-    except requests.exceptions.RequestException:
-        print('error for get a response from server')
+    return requests.get(url).ok
 
 
 def get_domain_name(url):
@@ -46,11 +43,13 @@ def is_expiration_date_ok(days_before_expiration, days_limit=30):
 
 def print_check_message(condition_stat, condition_exp, domain, days_before_exp):
     if condition_stat and condition_exp:
-        check_value = 'OK!'
+        check_value = 'SUCCESS!'
+        response = 'OK'
     else:
         check_value = 'FAILED!'
+        response = 'NO RESPONSE'
     print('for domain "{}"  :  check status: {}'.format(domain, check_value))
-    print('server response : {}'.format(check_value))
+    print('server response : {}'.format(response))
     print('days before expiration : {}'.format(days_before_exp))
 
 
@@ -63,7 +62,10 @@ if __name__ == '__main__':
         exit('incorrect path to file')
     urls_for_check_list = load_urls4check(filepath)
     for url in urls_for_check_list:
-        condition_status = is_server_respond_ok(url)
+        try:
+            condition_status = is_server_respond_ok(url)
+        except requests.exceptions.RequestException:
+            print('error for get a response from server')
         domain = get_domain_name(url)
         exp_date = get_domain_expiration_date(domain)
         days_before_exp = get_days_before_expiration(exp_date)
